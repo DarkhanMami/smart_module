e0295315bcd1e2016d337366de9def54aa2fd519
unknown <MaratDosmukhanov@gmail.com>
2019-05-07 23:29:56 +0600

    change

--- app/pages/components/mail/mailMessages.js
+++ app/pages/components/mail/mailMessages.js
@@ -13,11 +13,11 @@
     var messages = [
       {
         "id": "4563faass",
-        "name": "Nasta Linnie",
-        "subject": "Great text",
+        "name": "Интелектуальный менеджер",
+        "subject": "ПРС",
         "date": "2015-08-28T07:57:09",
         "body": $sce.trustAsHtml("<p>Hey John, </p><p>Check out this cool text.</p>"),
-        "pic": "img/Nasta.png",
+        "pic": "img/pp.png",
         "email": "petraramsey@mail.com",
         "attachment": "poem.txt",
         "position": "Great Employee",
@@ -26,13 +26,13 @@
       },
       {
         "id": "4563fdfvd",
-        "name": "Nasta Linnie",
-        "subject": "Lores ipsum",
+        "name": "Интелектуальный менеджер",
+        "subject": "КРС",
         "date": "2015-11-19T03:30:45",
         "important": false,
         "body": $sce.trustAsHtml("<p>Hey John, </p><br><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris ex mauris, ultrices vel lectus quis, scelerisque hendrerit ipsum. Suspendisse ullamcorper turpis neque, eget dapibus magna placerat ac. Suspendisse rhoncus ligula ac mi tempus varius ut sed lacus. Sed et commodo nulla, et placerat leo. Nam rhoncus vulputate sem non pharetra. Praesent fringilla massa in laoreet convallis. Aliquam lobortis dui a congue facilisis. Aenean dapibus semper semper. Quisque aliquam, nibh dapibus interdum condimentum, ex velit tempor tortor, at vestibulum magna leo quis leo. Morbi pulvinar varius erat ac rutrum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; In hac habitasse platea dictumst.</p>" +
           "<br><p>Cras rhoncus quam ipsum, vel dignissim nisl egestas sed. Aliquam erat volutpat. Integer eu nisl elit. Donec malesuada diam vitae tellus luctus tincidunt. Donec tempus blandit neque, rutrum egestas ipsum sagittis tempor. Curabitur volutpat ligula enim, nec vehicula purus molestie at. Sed a facilisis enim, nec molestie magna. Donec in augue non est viverra dapibus vel tempus risus. Nam porttitor purus sit amet hendrerit ullamcorper. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.</p>"),
-        "pic": "img/Nasta.png",
+        "pic": "img/pp.png",
         "email": "petraramsey@mail.com",
         "position": "Great Employee",
         "tag": "study",
@@ -40,12 +40,12 @@
       },
       {
         "id": "4563zxcss",
-        "name": "Nasta Linnie",
-        "subject": "Lores ipsum",
+        "name": "Интелектуальный менеджер",
+        "subject": "КРС",
         "date": "2015-10-19T03:30:45",
         "important": false,
         "body": $sce.trustAsHtml("<p>Hey Nasta, </p><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>"),
-        "pic": "img/Nasta.png",
+        "pic": "img/pp.png",
         "email": "petraramsey@mail.com",
         "position": "Great Employee",
         "tag": "work",
@@ -53,12 +53,12 @@
       },
       {
         "id": "8955sddf",
-        "name": "Nick Cat",
-        "subject": "New Design",
+        "name": "Интелектуальный менеджер",
+        "subject": "ГРП",
         "date": "2015-05-05T12:59:45",
         "body": $sce.trustAsHtml("<p>Hey John, Consectetur adipiscing elit</p><br>" +
           "<p>Cras rhoncus quam ipsum, vel dignissim nisl egestas sed. Aliquam erat volutpat. Integer eu nisl elit. Donec malesuada diam vitae tellus luctus tincidunt. Donec tempus blandit neque, rutrum egestas ipsum sagittis tempor. Curabitur volutpat ligula enim, nec vehicula purus molestie at. Sed a facilisis enim, nec molestie magna. Donec in augue non est viverra dapibus vel tempus risus. Nam porttitor purus sit amet hendrerit ullamcorper. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.</p>"),
-        "pic": "img/Nick.png",
+        "pic": "img/pp.png",
         "email": "barlowshort@mail.com",
         "position": "Graphical designer",
         "attachment": "design.psd",
@@ -67,11 +67,11 @@
       },
       {
         "id": "8955sdfcc",
-        "name": "Nick Cat",
-        "subject": "Gift card",
+        "name": "Интелектуальный менеджер",
+        "subject": "ТРС",
         "date": "2015-07-18T10:19:01",
         "body": $sce.trustAsHtml("<p>Hey John, </p><br><p>Consectetur adipiscing elit, Lorem ipsum dolor sit amet</p>"),
-        "pic": "img/Nick.png",
+        "pic": "img/pp.png",
         "email": "barlowshort@mail.com",
         "position": "Graphical designer",
         "tag": "study",
@@ -79,11 +79,11 @@
       },
       {
         "id": "8955asewf",
-        "name": "Nick Cat",
-        "subject": "Some news",
+        "name": "Интелектуальный менеджер",
+        "subject": "ПРС",
         "date": "2015-09-23T03:04:10",
         "body": $sce.trustAsHtml("<p>Hey John, </p><br><p>Integer eu nisl elit. Donec malesuada diam vitae tellus luctus tincidunt. Donec tempus blandit neque, rutrum egestas ipsum sagittis tempor. Curabitur volutpat ligula enim, nec vehicula purus molestie at. Sed a facilisis enim, nec molestie magna. Donec in augue non est viverra dapibus vel tempus risus. Nam porttitor purus sit amet hendrerit ullamcorper. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.</p>"),
-        "pic": "img/Nick.png",
+        "pic": "img/pp.png",
         "email": "barlowshort@mail.com",
         "position": "Graphical designer",
         "tag": "work",
@@ -91,15 +91,15 @@
       },
       {
         "id": "2334uudsa",
-        "name": "Kostya Danovsky",
-        "subject": "Street Art",
+        "name": "Интелектуальный менеджер",
+        "subject": "КРС",
         "date": "2015-11-22T10:05:09",
-        "body": $sce.trustAsHtml("<p>Hey John, </p><p>Aliquam eu facilisis eros, quis varius est.</p>" +
-          "<p>Consectetur adipiscing elit. Aliquam sodales sem in nibh pellentesque, ac dignissim mi dapibus.</p>" +
-          "<p>Lorem ipsum dolor sit amet! Nullam imperdiet justo a ipsum laoreet euismod.</p>" +
-          "<br><p>Cras tincidunt fermentum lectus, quis scelerisque lorem volutpat sed." +
-          "Sed quis orci sed nisl sagittis viverra id at mauris. Nam venenatis mi nibh. Sed fringilla mattis vehic</p>"),
-        "pic": "img/Kostya.png",
+        "body": $sce.trustAsHtml("<p>Уважаемый Жанбек Бердибекович, </p><br><p>Скважина № 172 м/р Жанаталап.</p>" +
+          "<p>В период с 1.09.2018 г. по 8.09.2018 г. по данным системы ИСУ СДМО при неизменном коэффициенте наполнения насоса наблюдается резкое снижение добычи жидкости. Причиной снижения добычи с наибольшей вероятностью являются утечки в насосной оборудовании (утечки в резьбовых соединениях труб, в зазоре между плунжером и цилиндром, в клапанах).</p>" +
+          "<p>Учитывая отсутствие других факторов, влияющих на снижение добычи жидкости, я рекомендую произвести осмотр насосного оборудования на предмет утечек.</p>" +
+          "<br><p>Дата последнего ремонта насоса на скважине: 1.02.2018," +
+          "<br>Рекомендуемая дата ремонта насоса на скважине: 10.09.2018</p>"),
+        "pic": "img/pp.png",
         "email": "schwart@mail.com",
         "position": "Technical Chef",
         "attachment": "file.doc",
@@ -108,15 +108,15 @@
       },
       {
         "id": "2334aefvv",
-        "name": "Kostya Danovsky",
-        "subject": "New product",
+        "name": "Интелектуальный менеджер",
+        "subject": "Другое",
         "date": "2015-06-22T06:26:10",
         "body": $sce.trustAsHtml("<p>Hello John, </p><p>Lorem ipsum dolor sit amet!</p>" +
           "<p>Consectetur adipiscing elit. Aliquam sodales sem in nibh pellentesque, ac dignissim mi dapibus.</p>" +
           "<p>Aliquam eu facilisis eros, quis varius est. Nullam imperdiet justo a ipsum laoreet euismod.</p>" +
           "<br><p>Nulla facilisi. Nulla congue, arcu eget blandit lacinia, leo ante ullamcorper lectus, vel pulvinar justo ipsum vitae justo." +
           "Cras tincidunt fermentum lectus, quis scelerisque lorem volutpat sed. Sed quis orci sed nisl sagittis viverra id at mauris. Nam venenatis mi nibh. Sed fringilla mattis vehic</p>"),
-        "pic": "img/Kostya.png",
+        "pic": "img/pp.png",
         "email": "schwart@mail.com",
         "position": "Technical Chef",
         "tag": "family",
@@ -124,14 +124,14 @@
       },
       {
         "id": "2334cvdss",
-        "name": "Kostya Danovsky",
-        "subject": "Old product",
+        "name": "Интелектуальный менеджер",
+        "subject": "ПРС",
         "date": "2015-06-22T06:26:10",
         "body": $sce.trustAsHtml("<p>Hello John, </p>" +
           "<p>Consectetur adipiscing elit. Aliquam sodales sem in nibh pellentesque, ac dignissim mi dapibus.</p>" +
           "<br>"+
           "<p>Cras tincidunt fermentum lectus, quis scelerisque lorem volutpat sed. Sed quis orci sed nisl sagittis viverra id at mauris. Nam venenatis mi nibh. Sed fringilla mattis vehic</p>"),
-        "pic": "img/Kostya.png",
+        "pic": "img/pp.png",
         "email": "schwart@mail.com",
         "position": "Technical Chef",
         "tag": "study",
@@ -139,12 +139,12 @@
       },
       {
         "id": "8223xzxfn",
-        "name": "Andrey Hrabouski",
-        "subject": "Skype moji",
+        "name": "Интелектуальный менеджер",
+        "subject": "Другое",
         "date": "2015-07-16T06:47:53",
         "body": $sce.trustAsHtml("<p>Hello John, </p><p>Aliquam sodales sem in nibh pellentesque</p>" +
           "<p>Lorem ipsum dolor I find moji in skype sit amet!.</p>"),
-        "pic": "img/Andrey.png",
+        "pic": "img/pp.png",
         "email": "lakeishaphillips@mail.com",
         "position": "Mobile Developer",
         "tag": 'family',
@@ -152,12 +152,12 @@
       },
       {
         "id": "8223sdffn",
-        "name": "Andrey Hrabouski",
-        "subject": "My App",
+        "name": "Интелектуальный менеджер",
+        "subject": "ПРС",
         "date": "2015-06-20T07:05:02",
         "body": $sce.trustAsHtml("<p>Hey Vlad. </p><p>Lorem ipsum dolor sit amet!</p>" +
           "<p>Consectetur My Falasson App elit. Aliquam sodales sem in nibh pellentesque, ac dignissim mi dapibus.</p>"),
-        "pic": "img/Andrey.png",
+        "pic": "img/pp.png",
         "email": "lakeishaphillips@mail.com",
         "position": "Mobile Developer",
         "tag": 'family',
@@ -165,12 +165,12 @@
       },
       {
         "id": "9391xdsff",
-        "name": "Vlad Lugovsky",
-        "subject": "Cool",
+        "name": "Интелектуальный менеджер",
+        "subject": "КРС",
         "date": "2015-03-31T11:52:58",
         "body": $sce.trustAsHtml("<p>Hey Vlad. </p><p>Aliquam sodales sem in nibh pellentesque</p>" +
           "<p>Cras tincidunt fermentum lectus, quis scelerisque lorem volutpat sed.</p>"),
-        "pic": "img/Vlad.png",
+        "pic": "img/pp.png",
         "email": "carlsongoodman@mail.com",
         "position": "Fullstack man",
         "tag": "study",
@@ -178,12 +178,12 @@
       },
       {
         "id": "8223xsdaa",
-        "name": "Andrey Hrabouski",
-        "subject": "Car rent",
+        "name": "Интелектуальный менеджер",
+        "subject": "ТРС",
         "date": "2015-02-25T10:58:58",
         "body": $sce.trustAsHtml("<p>Hey Andrey. </p>" +
           "<p>Cras tincidunt fermentum lectus, quis scelerisque lorem volutpat sed. Sed quis orci sed nisl sagittis viverra id at mauris. Nam venenatis mi nibh. Sed fringilla mattis vehic</p>"),
-        "pic": "img/Andrey.png",
+        "pic": "img/pp.png",
         "email": "lakeishaphillips@mail.com",
         "position": "Mobile Developer",
         "tag": "family",
@@ -191,12 +191,12 @@
       },
       {
         "id": "9391xdsff",
-        "name": "Vlad Lugovsky",
-        "subject": "What next",
+        "name": "Интелектуальный менеджер",
+        "subject": "ГРП",
         "date": "2015-03-31T11:52:58",
         "body": $sce.trustAsHtml("<p>Hey Vlad. </p><p>Lorem ipsum dolor sit amet!</p>" +
           "<p>Esse esse labore tempor ullamco ullamco. Id veniam laborum c.</p>"),
-        "pic": "img/Vlad.png",
+        "pic": "img/pp.png",
         "email": "carlsongoodman@mail.com",
         "position": "Fullstack man",
         "tag": "study",
@@ -208,24 +208,24 @@
       }).reverse();
     var tabs = [{
       label: 'inbox',
-      name: 'Inbox',
+      name: 'Новые',
       newMails: 7
     }, {
       label: 'sent',
-      name: 'Sent Mail'
+      name: 'В процессе'
     }, {
       label: 'important',
-      name: 'Important'
+      name: 'Важные'
     }, {
       label: 'draft',
-      name: 'Draft',
+      name: 'Закрытые',
       newMails: 2
     }, {
       label: 'spam',
-      name: 'Spam'
+      name: 'Спам'
     }, {
       label: 'trash',
-      name: 'Trash'
+      name: 'Мусор'
     }];
 
     return{
