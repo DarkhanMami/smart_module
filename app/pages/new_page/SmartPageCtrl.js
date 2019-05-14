3ce6641c963511af9d56be322005da2c6d50664b
unknown <MaratDosmukhanov@gmail.com>
2019-05-15 05:39:05 +0600

    change

--- app/pages/new_page/SmartPageCtrl.js
+++ app/pages/new_page/SmartPageCtrl.js
@@ -1474,6 +1474,30 @@
         return result;
     }
 
+    var Accordion = function(el, multiple) {
+      this.el = el || {};
+      this.multiple = multiple || false;
+
+      // Variables privadas
+      var links = this.el.find('.link');
+      // Evento
+      links.on('click', {el: this.el, multiple: this.multiple}, this.dropdown)
+    }
+    Accordion.prototype.dropdown = function(e) {
+
+        var $el = e.data.el;
+        var $this = $(this),
+        $next = $this.next();
+
+        $next.slideToggle();
+        $this.parent().toggleClass('open');
+
+        if (!e.data.multiple) {
+          $el.find('.submenu').not($next).slideUp().parent().removeClass('open');
+        };
+    } 
+    var accordion = new Accordion($('#accordion'), false);
+
 
     $scope.removeUser = function(index) {
       $scope.users.splice(index, 1);
